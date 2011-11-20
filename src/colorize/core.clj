a6c40d23954cfb9d2f656abb95ded9018d6a3195
Chris Granger <ibdknox@gmail.com>
2011-11-19 20:07:20 -0800

    add in other unicode types for underline/bold/etc
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- src/colorize/core.clj
+++ src/colorize/core.clj
@@ -10,7 +10,20 @@
                   :blue    "[34m"
                   :yellow  "[33m"
                   :magenta "[35m"
-                  :cyan    "[36m"})
+                  :cyan    "[36m"
+                  :black-bg "[40m"
+                  :red-bg "[41m"
+                  :green-bg "[42m"
+                  :yellow-bg "[43m"
+                  :blue-bg "[44m"
+                  :magenta-bg "[45m"
+                  :cyan-bg "[46m"
+                  :white-bg "[47m"
+                  :bold "[1m"
+                  :italic "[3m"
+                  :underline "[4m"
+                  :inverse "[7m"
+                  :strikethrough "[9m"})
 
 (defn ansi
   "Get the ansi code for a specific color"
@@ -21,7 +34,9 @@
   "Wrap the given strings in the provided color. Color should be
   a keyword and can be any of the following:
   
-  [:reset :default :white :black :red :green :blue :yellow :magenta :cyan].
+  [:reset :default :white :black :red :green :blue :yellow :magenta :cyan
+   :black-bg :red-bg :green-bg :yellow-bg :blue-bg :magenta-bg :cyan-bg :white-bg
+   :underline :italic :bold :strikethrough :inverse].
 
   Each of these also has a function created for it: (cyan \"woohoo\")"
   [code & s]
